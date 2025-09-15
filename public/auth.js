9ff5775ed1c105a4f6c67159bed3e125c5cddfe3
Mappledude <sablani.zane@gmail.com>
2025-09-15 15:30:46 -0400

    Add debug export packet with jamlog ring buffer

--- public/auth.js
+++ public/auth.js
@@ -1,6 +1,5 @@
 import { app } from "/firebase-init.js";
 import { getAuth, onAuthStateChanged, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { isDebug } from "/common.js";
 
 export const auth = getAuth(app);
 let currentUser = auth.currentUser;
@@ -13,18 +12,18 @@ onAuthStateChanged(auth, (u) => {
     currentUser = u;
     ready = true;
     readyResolve(u);
-    if (window.jamlog && isDebug()) {
+    if (window.jamlog) {
       window.jamlog.setUid(u.uid);
       window.jamlog.push('auth.ready', { uid: u.uid });
     }
   } else {
-    if (window.jamlog && isDebug()) window.jamlog.push('auth.signInAnonymously.start');
+    if (window.jamlog) window.jamlog.push('auth.signInAnonymously.start');
     signInAnonymously(auth)
       .then(() => {
-        if (window.jamlog && isDebug()) window.jamlog.push('auth.signInAnonymously.ok');
+        if (window.jamlog) window.jamlog.push('auth.signInAnonymously.ok');
       })
       .catch((e) => {
-        if (window.jamlog && isDebug()) {
+        if (window.jamlog) {
           window.jamlog.push('auth.signInAnonymously.fail', { code: e?.code, message: e?.message });
         }
         console.error(e);
